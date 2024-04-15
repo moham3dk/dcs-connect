a2b2a541a8010c0a62f525eea00030b684a184b7
Mohamed Khettab <93957717+mohamed-khettab@users.noreply.github.com>
2024-04-14 21:06:01 -0400

    Update commitToGithub.js

--- backend/commitToGithub.js
+++ backend/commitToGithub.js
@@ -8,9 +8,9 @@ function commitToGithub() {
   // Get the JSON content of the extracurriculars spreadsheet
   var content = convertToJSON();
 
-  // Temporarily comment out for testing purposes
-  // var commitUrl = githubClient.commit(content, filename, email);
-  // Logger.log("New commit URL: " + commitUrl);
+
+  var commitUrl = githubClient.commit(content, filename, email);
+  Logger.log("New commit URL: " + commitUrl);
 
   Logger.log('Commited Successfully!');
 }
