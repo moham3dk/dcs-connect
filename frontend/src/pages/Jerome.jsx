3695372ef9f6ecceb854a33fdb747a60c1fbb0f2
Mohamed Khettab <93957717+mohamed-khettab@users.noreply.github.com>
2024-05-16 22:19:51 -0400

    made it for all 3 schools

---
+++ frontend/src/pages/Jerome.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Display from '../components/Display'
+import Header from '../components/Header'
+import extracurricularData from '../data/extracurriculars/jerome_extracurriculars.json'
+
+const Jerome = () => {
+  return (
+    <>
+      <Header schoolName="JEROME" link={"https://google.com"}/>
+      <Display schoolName="JEROME" extracurricularData={extracurricularData}/>
+    </>
+  )
+}
+
+export default Jerome
